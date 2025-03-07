21e37d0f60ced20062e163e2c01605e208348c94
Rene-Roscher <43115138+Rene-Roscher@users.noreply.github.com>
2025-03-07 14:11:48 +0000

    Fix styling

--- src/Commands/LaravelI18nJsonConverterCommand.php
+++ src/Commands/LaravelI18nJsonConverterCommand.php
@@ -14,35 +14,37 @@ class LaravelI18nJsonConverterCommand extends Command
 
     public function __construct(
         protected Filesystem $files
-    )
-    {
+    ) {
         parent::__construct();
     }
 
     public function handle()
     {
         $langPath = resource_path('lang');
-        $jsonPath = $langPath . '/json';
+        $jsonPath = $langPath.'/json';
 
-        if (!File::exists($jsonPath))
+        if (! File::exists($jsonPath)) {
             File::makeDirectory($jsonPath, 0755, true);
+        }
 
         foreach ($this->files->directories($langPath) as $localeDir) {
             $locale = basename($localeDir);
 
-            if ($locale === 'json')
+            if ($locale === 'json') {
                 continue;
+            }
 
             foreach ($this->files->files($localeDir) as $file) {
                 if ($file->getExtension() === 'php') {
                     $translations = include $file->getPathname();
                     $flattened = $this->flattenArray($translations);
-                    $jsonFileName = $file->getBasename('.php') . '.json';
+                    $jsonFileName = $file->getBasename('.php').'.json';
                     $jsonLocalePath = "$jsonPath/$locale";
                     $jsonFilePath = "{$jsonLocalePath}/{$jsonFileName}";
 
-                    if (!File::exists($jsonLocalePath))
+                    if (! File::exists($jsonLocalePath)) {
                         File::makeDirectory($jsonLocalePath, 0755, true);
+                    }
 
                     $this->files->put($jsonFilePath, json_encode($flattened, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES));
                     $this->info("Converted $file to $jsonFilePath");
@@ -51,27 +53,25 @@ class LaravelI18nJsonConverterCommand extends Command
         }
 
         $this->info('All translations have been converted to JSON.');
+
         return 0;
     }
 
     /**
      * Flattens a multi-dimensional array with dot notation.
-     *
-     * @param array $array
-     * @param string $prefix
-     * @return array
      */
     protected function flattenArray(array $array, string $prefix = ''): array
     {
         $result = [];
         foreach ($array as $key => $value) {
-            $newKey = $prefix . $key;
+            $newKey = $prefix.$key;
             if (is_array($value)) {
-                $result = array_merge($result, $this->flattenArray($value, $newKey . '.'));
+                $result = array_merge($result, $this->flattenArray($value, $newKey.'.'));
             } else {
                 $result[$newKey] = $value;
             }
         }
+
         return $result;
     }
 }
